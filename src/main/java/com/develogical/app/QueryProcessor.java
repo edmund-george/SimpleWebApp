2556b7bb07585a6c8a998ff7ccdcdf2b46c6f1d8
edmund-george <e.george@gmail.com>
2020-02-20 10:11:02 +0000

    new search query

--- src/main/java/com/develogical/app/QueryProcessor.java
+++ src/main/java/com/develogical/app/QueryProcessor.java
@@ -7,11 +7,17 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
+        else
         if (query.toLowerCase().contains("softwareengineering")) {
             return "The act of getting software to do what you want it to do. (Edmund George)";
         }
+        else
         if (query.toLowerCase().contains("machinelearning")) {
             return "How we create SkyNet (Edmund George)";
+        }
+        else
+        if (query.toLowerCase().contains("ping")) {
+            return "pong";
         }
         return "";
     }
